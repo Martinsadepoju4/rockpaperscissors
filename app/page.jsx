49b39db58e99324a6cad37ecfb91f63976a9720b
Martinsadepoju4 <martinsadepoju4@gmail.com>
2024-01-11 09:42:06 -0800

    fixed draw media queries

--- app/page.jsx
+++ app/page.jsx
@@ -148,42 +148,36 @@ function Home() {
             width={0}
             height={0}
             style={{ width: "60vw", height: "auto" }}
-            onLoad={handleImageLoad}
           />
           <Avatar
             name="paper"
             function={handleSelections}
             class={styles.paper}
             containerclass={styles.paper_container}
-            onLoadfunction={handleImageLoad}
           />
           <Avatar
             function={handleSelections}
             name="rock"
             class={styles.rock}
             containerclass={styles.rock_container}
-            onLoadfunction={handleImageLoad}
           />
           <Avatar
             function={handleSelections}
             name="lizard"
             class={styles.lizard}
             containerclass={styles.lizard_container}
-            onLoadfunction={handleImageLoad}
           />
           <Avatar
             function={handleSelections}
             name="spock"
             class={styles.spock}
             containerclass={styles.spock_container}
-            onLoadfunction={handleImageLoad}
           />
           <Avatar
             function={handleSelections}
             name="scissors"
             class={styles.scissors}
             containerclass={styles.scissors_container}
-            onLoadfunction={handleImageLoad}
           />
         </div>
 
